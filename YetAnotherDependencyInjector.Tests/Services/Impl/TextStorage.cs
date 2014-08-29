9f9425a312c807105c47c4dca8564964adae728c
luisxkimo <luisxkimo@gmail.com>
2014-08-29 14:51:25 +0200

    Añadidas funcionalidades de registro de nombres por implementación y resolución de varios parametros también por nombre

---
+++ YetAnotherDependencyInjector.Tests/Services/Impl/TextStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace YetAnotherDependencyInjector.Tests.Services.Impl
+{
+    public class TextStorage : IDataStorage
+    {
+        public void Save(string note)
+        {
+            using (var writer = new StreamWriter("db.txt"))
+            {
+                writer.WriteLine(note);
+            }
+        }
+    }
+}
